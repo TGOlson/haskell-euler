5dc7f4200e11c8a4f743622ef1a3c8eab2015d8a
Tyler Olson <tydotg@gmail.com>
2015-04-13 20:54:21 -0700

    Use sieves

--- lib/Primes.hs
+++ lib/Primes.hs
@@ -1,9 +1,9 @@
 module Primes where
 
 import Data.Array.Unboxed
---
--- a :: (IArray a e, Ix i) => a i e
--- a = array (1,100) ((1,1) : [(i, i * a!(i-1)) | i <- [2..100]])
+
+type SieveArray = UArray Int Bool
+
 
 primes :: [Int]
 primes = 2 : filter isPrime [3,5..]
@@ -17,36 +17,31 @@ sqrtInt = ceiling . sqrt . fromIntegral
 takePrimes :: Int -> [Int]
 takePrimes x = take x primes
 
-takePrimesBelow :: Int -> [Int]
-takePrimesBelow x = takeWhile (< x) primes
-
 nthPrimeNumber :: Int -> Int
 nthPrimeNumber = last . takePrimes
 
-sieveArray :: Int -> UArray Int Bool
+
+takePrimesBelow :: Int -> [Int]
+takePrimesBelow = map fst . filter ((== True) . snd) . assocs . sieve 2 . sieveArray
+
+sieveArray :: Int -> SieveArray
 sieveArray n = listArray (1, n) (False:(replicate (n - 1) True))
 
+sieveLength :: SieveArray -> Int
+sieveLength = snd . bounds
 
-
--- let a = array (0, 3) (replicate 4 True) :: UArray Int Bool
-
--- Use list of bools
-sieve :: Int -> [Int]
-sieve = map fst . filter ((== True) . snd) . assocs . sieve' 2 . sieveArray
-
---
-sieve' :: Int -> UArray Int Bool -> UArray Int Bool
-sieve' n ps
-  | n > uLength ps = ps
-  | otherwise = sieve' (n + 1) nextPrimes
+sieve :: Int -> SieveArray -> SieveArray
+sieve n ps
+  | n > sqrtInt (sieveLength ps) = ps
+  | otherwise = sieve (nextFactor n nextPrimes) nextPrimes
   where nextPrimes = replaceIndexFactorsOf n ps
-  -- naive - uses n + 1 for next number to interigate, should use next 'true' value
 
-uLength :: UArray Int Bool -> Int
-uLength = length . elems
+replaceIndexFactorsOf :: Int -> SieveArray -> SieveArray
+replaceIndexFactorsOf n array = (//) array [(i, False) | i <- [(n * 2), (n * 3)..(sieveLength array)]]
 
-replaceIndexFactorsOf :: Int -> UArray Int Bool -> UArray Int Bool
-replaceIndexFactorsOf n array = (//) array [(i, False) | i <- [(n * 2), (n * 3)..(uLength array)]]
+nextFactor :: Int -> SieveArray -> Int
+nextFactor n ps
+  | n > sqrtInt (sieveLength ps) = n + 1
+  | ps ! (n + 1) == True = n + 1
+  | otherwise = nextFactor (n + 1) ps
 
-reject :: (Int -> Bool) -> [Int] -> [Int]
-reject f = filter $ not . f
