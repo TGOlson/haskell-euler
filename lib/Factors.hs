f3fb027178d49d5595a900c1adc270aad3fde7f9
Tyler Olson <tydotg@gmail.com>
2015-04-20 13:36:29 -0700

    Cleanup

--- lib/Factors.hs
+++ lib/Factors.hs
@@ -19,16 +19,9 @@ isFactorOf :: Int -> Int -> Bool
 isFactorOf x y = y `mod` x == 0
 
 commonFactors :: Int -> Int -> [Int]
-commonFactors x y = intersect (getFactors x) (getFactors y)
+commonFactors x y = getFactors x `intersect` getFactors y
 
 primeFactors :: Int -> [Int]
-primeFactors = getPrimeFactorsWithContainer []
-
--- try to refactor this into a single function
-getPrimeFactorsWithContainer :: [Int] -> Int -> [Int]
-getPrimeFactorsWithContainer xs 1 = xs
-getPrimeFactorsWithContainer xs x = getPrimeFactorsWithContainer xs' x'
-    where
-      nextPrimeFactor = head [y | y <- primes, isFactorOf y x]
-      x' = x `div` nextPrimeFactor
-      xs' = nextPrimeFactor:xs
+primeFactors 1 = []
+primeFactors x = factor : primeFactors (x `div` factor)
+  where factor = head [y | y <- primes, isFactorOf y x]
--- lib/Primes.hs
+++ lib/Primes.hs
@@ -20,12 +20,11 @@ takePrimes x = take x primes
 nthPrimeNumber :: Int -> Int
 nthPrimeNumber = last . takePrimes
 
-
 takePrimesBelow :: Int -> [Int]
 takePrimesBelow = map fst . filter ((== True) . snd) . assocs . sieve 2 . sieveArray
 
 sieveArray :: Int -> SieveArray
-sieveArray n = listArray (1, n) (False:(replicate (n - 1) True))
+sieveArray n = listArray (1, n) (False: replicate (n - 1) True)
 
 sieveLength :: SieveArray -> Int
 sieveLength = snd . bounds
@@ -37,11 +36,10 @@ sieve n ps
   where nextPrimes = replaceIndexFactorsOf n ps
 
 replaceIndexFactorsOf :: Int -> SieveArray -> SieveArray
-replaceIndexFactorsOf n array = (//) array [(i, False) | i <- [(n * 2), (n * 3)..(sieveLength array)]]
+replaceIndexFactorsOf n a = (//) a [(i, False) | i <- [(n * 2), (n * 3)..(sieveLength a)]]
 
 nextFactor :: Int -> SieveArray -> Int
 nextFactor n ps
   | n > sqrtInt (sieveLength ps) = n + 1
-  | ps ! (n + 1) == True = n + 1
+  | ps ! (n + 1) = n + 1
   | otherwise = nextFactor (n + 1) ps
-
