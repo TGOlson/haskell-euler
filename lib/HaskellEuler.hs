11929150fedde1f8bd22c13c14aaac496ab74a96
Tyler Olson <tydotg@gmail.com>
2016-08-03 16:56:51 -0700

    kewl cleanup

--- lib/HaskellEuler.hs
+++ lib/HaskellEuler.hs
@@ -1,80 +1,38 @@
 module HaskellEuler where
 
+import Data.Monoid
 import System.Environment
+import HaskellEuler.AnswerSets
 
-import qualified HaskellEuler.Problems.P1  as P1
-import qualified HaskellEuler.Problems.P2  as P2
-import qualified HaskellEuler.Problems.P3  as P3
-import qualified HaskellEuler.Problems.P4  as P4
-import qualified HaskellEuler.Problems.P5  as P5
-import qualified HaskellEuler.Problems.P6  as P6
-import qualified HaskellEuler.Problems.P7  as P7
-import qualified HaskellEuler.Problems.P8  as P8
-import qualified HaskellEuler.Problems.P9  as P9
-import qualified HaskellEuler.Problems.P10 as P10
-import qualified HaskellEuler.Problems.P11 as P11
-import qualified HaskellEuler.Problems.P12 as P12
-import qualified HaskellEuler.Problems.P13 as P13
--- import qualified HaskellEuler.Problems.P14 as P14
-import qualified HaskellEuler.Problems.P15 as P15
-import qualified HaskellEuler.Problems.P16 as P16
+showAnswerFormatted :: Int -> AnswerSet -> String
+showAnswerFormatted n answerSet =
+    mconcat [ "Problem ", show n, " - "
+            , showAnswer answerSet, " "
+            , showTag answerSet
+            ]
 
+showAnswer :: AnswerSet -> String
+showAnswer = \case Answered x _ -> show x
+                   UnVerified x -> show x
+                   _            -> ""
 
-data AnswerSet = AnswerSet Int (Maybe Int) (Maybe Int)
+showTag :: AnswerSet -> String
+showTag = \case Answered x y | x == y -> "[solved]"
+                Answered _ _          -> "[unsolved]"
+                UnVerified _          -> "[unverified]"
+                UnAnswered            -> ""
+                Skip reason _         -> "[skipping - " <> reason <> "]"
 
-answerSets :: [AnswerSet]
-answerSets =
-    [ AnswerSet 1  (Just P1.answer)  (Just 233168)
-    , AnswerSet 2  (Just P2.answer)  (Just 4613732)
-    , AnswerSet 3  (Just P3.answer)  (Just 6857)
-    , AnswerSet 4  (Just P4.answer)  (Just 906609)
-    , AnswerSet 5  (Just P5.answer)  (Just 232792560)
-    , AnswerSet 6  (Just P6.answer)  (Just 25164150)
-    , AnswerSet 7  (Just P7.answer)  (Just 104743)
-    , AnswerSet 8  (Just P8.answer)  (Just 23514624000)
-    , AnswerSet 9  (Just P9.answer)  (Just 31875000)
-    , AnswerSet 10 (Just P10.answer) (Just 142913828922)
-    , AnswerSet 11 (Just P11.answer) (Just 70600674)
-    , AnswerSet 12 (Just P12.answer) (Just 76576500)
-    , AnswerSet 13 (Just P13.answer) (Just 5537376230)
-    -- , AnswerSet 14 (Answer P14.answer) (Answer 837799)) -- runs slow
-    , AnswerSet 14 Nothing Nothing
-    , AnswerSet 15 (Just P15.answer) (Just 137846528820)
-    , AnswerSet 16 (Just P16.answer) (Just 1366)
-    , AnswerSet 17 Nothing               Nothing
-    , AnswerSet 18 Nothing               Nothing
-    , AnswerSet 19 Nothing               Nothing
-    , AnswerSet 20 Nothing               Nothing
-    ]
-
-showAnswerFormatted :: AnswerSet -> String
-showAnswerFormatted (AnswerSet n answer solution) =
-  showProblem n ++ " - " ++
-  show answer ++ " " ++
-  showState answer solution
-
-showProblem :: Int -> String
-showProblem n = "Problem " ++ show n
-
-showState :: Maybe Int -> Maybe Int -> String
-showState Nothing _ = ""
-showState _ Nothing = "[unverified]"
-showState x y = if x == y then "[solved]" else "[unsolved]"
 
 
 printAnswerSets :: [AnswerSet] -> IO ()
-printAnswerSets = mapM_ printAnswerSet
+printAnswerSets = mapM_ putStrLn . zipWith showAnswerFormatted [1..]
 
-printAnswerSet :: AnswerSet -> IO ()
-printAnswerSet = putStrLn . showAnswerFormatted
 
 printAnswerSetIndex :: Int -> IO ()
-printAnswerSetIndex x = printAnswerSets [answerSets !! (x - 1)]
-
-dispatch :: [String] -> IO ()
-dispatch =
-    \case "p":args -> printAnswerSetIndex . read $ head args
-          _        -> printAnswerSets answerSets
+printAnswerSetIndex x = putStrLn $ showAnswerFormatted x $ answerSets !! (x - 1)
 
 main :: IO ()
-main = getArgs >>= dispatch
+main = getArgs >>= \case
+    "p":x:_ -> printAnswerSetIndex (read x)
+    _       -> printAnswerSets answerSets
---
+++ lib/HaskellEuler/AnswerSets.hs
@@ -0,0 +1,54 @@
+module HaskellEuler.AnswerSets
+    ( answerSets
+    , AnswerSet(..)
+    ) where
+
+
+import qualified HaskellEuler.Problems.P1  as P1
+import qualified HaskellEuler.Problems.P2  as P2
+import qualified HaskellEuler.Problems.P3  as P3
+import qualified HaskellEuler.Problems.P4  as P4
+import qualified HaskellEuler.Problems.P5  as P5
+import qualified HaskellEuler.Problems.P6  as P6
+import qualified HaskellEuler.Problems.P7  as P7
+import qualified HaskellEuler.Problems.P8  as P8
+import qualified HaskellEuler.Problems.P9  as P9
+import qualified HaskellEuler.Problems.P10 as P10
+import qualified HaskellEuler.Problems.P11 as P11
+import qualified HaskellEuler.Problems.P12 as P12
+import qualified HaskellEuler.Problems.P13 as P13
+import qualified HaskellEuler.Problems.P14 as P14
+import qualified HaskellEuler.Problems.P15 as P15
+import qualified HaskellEuler.Problems.P16 as P16
+-- import qualified HaskellEuler.Problems.P20 as P20
+
+data AnswerSet
+    = Answered Int Int
+    | UnVerified Int
+    | UnAnswered
+    | Skip String AnswerSet
+
+
+answerSets :: [AnswerSet]
+answerSets =
+    [ Answered P1.answer  233168
+    , Answered P2.answer  4613732
+    , Answered P3.answer  6857
+    , Answered P4.answer  906609
+    , Answered P5.answer  232792560
+    , Answered P6.answer  25164150
+    , Answered P7.answer  104743
+    , Answered P8.answer  23514624000
+    , Skip "slow" $ Answered P9.answer 31875000
+    , Answered P10.answer 142913828922
+    , Answered P11.answer 70600674
+    , Answered P12.answer 76576500
+    , Answered P13.answer 5537376230
+    , Skip "slow" $ Answered P14.answer 837799
+    , Answered P15.answer 137846528820
+    , Answered P16.answer 1366
+    , UnAnswered
+    , UnAnswered
+    , UnAnswered
+    , UnAnswered
+    ]
